ad655289cdfc3cc06fece8866fcaf596070856eb
Andrzej Lach <zastosowania@gmail.com>
2019-06-17 15:31:13 +0200

    beautify: make all with BEM ...

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Witaj przybszu waidzemyh cożeśty uważny reader - zdrów! ');
+console.log('Witaj przybszu waidzemyh cożeśty zacnie uważny reader - zdrów! ');
+
+const mainHeader = document.querySelector('.mainHeader');
+
+console.log(mainHeader);
+const paragraps = document.querySelectorAll('p');
+
+console.log(paragraps);
+console.log(paragraps[0]);
+
+const aj = document.querySelector('.ajax--js');
+console.log(aj.innerHTML);
+
+aj.innerHTML = '<strong>Hejka!</strong>';
+console.log(aj.innerHTML);
+
